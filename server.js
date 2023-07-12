798d10abce1b96310985655ee88f4f09385c45c0
Victor del Rosal <45711996+victordelrosal@users.noreply.github.com>
2023-07-12 08:14:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/chat', async (req, res) => {
-    const prompt = req.body.prompt;
+    const userMessage = req.body.message;
+    const messages = [
+        {role: "system", content: "You are a helpful assistant."},
+        {role: "user", content: userMessage},
+    ];
     const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        prompt: prompt,
-        max_tokens: 150,
+        messages: messages,
     }, {
         headers: {
             'Authorization': `Bearer ${process.env.OPENAI_SECRET_KEY}`,
             'Content-Type': 'application/json'
         }
     });
-    res.json(response.data.choices[0].text.trim());
+    res.json(response.data.choices[0].message.content);
 });
 
 const port = process.env.PORT || 5000;
